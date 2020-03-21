5c382a003b524d4c90dca1f65e80118d45bcbfec
RohithV <rohithvegesna@gmail.com>
2020-03-21 16:22:41 -0500

    First Commit
    Dispensor Experiance

---
+++ main.py
@@ -0,0 +1,17 @@
+from pump import *
+import sys
+
+
+class MyWin(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    myapp = MyWin()
+    myapp.show()
+    sys.exit(app.exec_())
