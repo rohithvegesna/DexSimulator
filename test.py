e53229040f0aa6b5688e924e4eb8632f0110db87
RohithVegesna <rohithvarma.vegesna@7-11.com>
2020-03-23 14:08:12 -0500

    test working

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
 import serial
 
 serialport = serial.Serial("COM2", 9600, timeout=0.5)
-rcvd = []
+my_bytes = b'\x02\x03\x76\x35\x30\x2e\x30\x2e\x35'
 while True:
     c = serialport.read()
-    if len(rcvd) != 0 and len(c) == 0:
-        print(rcvd)
-        break
     if len(c) != 0:
-        rcvd.append(c)
+        print(c)
+        serialport.write(my_bytes)
